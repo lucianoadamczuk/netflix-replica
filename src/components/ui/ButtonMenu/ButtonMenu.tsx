219f48f9a0ded238ed6857b4279c9da4a8dfa0c3
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2023-12-01 19:25:07 -0300

    refeature(ButtonMenu): delate animation

--- src/components/ui/ButtonMenu/ButtonMenu.tsx
+++ src/components/ui/ButtonMenu/ButtonMenu.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useWidthDetector } from '../../../hooks/useWidthDetector'
 import './ButtonMenu.css'
-import { useLocation } from 'react-router-dom'
 
 interface ButtonProps {
   onClick: () => void
@@ -9,22 +8,12 @@ interface ButtonProps {
 
 export const ButtonMenu: React.FC<ButtonProps> = ({ onClick }) => {
   const { isMobile } = useWidthDetector()
-  const [animation, setAnimation] = useState<boolean>(false)
-
-  useEffect(() => {
-    setAnimation(!animation)
-  }, [onClick])
-
-  const location = useLocation()
-  useEffect(() => {
-    setAnimation(false)
-  }, [location.pathname])
 
   return (
-    <article className={`ButtonMenu ${animation ? 'ButtonMenuAnimation' : ''} `} style={{ display: isMobile ? 'flex' : 'none' }} onClick={onClick}>
-        <div className="bar bar-1"></div>
-        <div className="bar bar-2"></div>
-        <div className="bar bar-3"></div>
+    <article className='ButtonMenu ' style={{ display: isMobile ? 'flex' : 'none' }} onClick={onClick}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
     </article>
   )
 }
