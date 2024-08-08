357ba3c3f40eaf0e97736596018a7e18a88063fc
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-08-08 17:18:51 -0300

    feat(components): :sparkles: create Spinner

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from "./Button/Button";
 export { default as CarouselResponsive } from "./CarouselResponsive/CarouselResponsive";
+export { default as SlideMedia } from "./SlideMedia/SlideMedia";
+export { default as Spinner } from "./Spinner/Spinner";
--- src/pages/PageMedia/PageMedia.tsx
+++ src/pages/PageMedia/PageMedia.tsx
@@ -1,9 +1,9 @@
 import { SwiperSlide } from "swiper/react";
+import { v4 as uuidv4 } from "uuid";
 import { CarouselResponsive } from "../../components";
+import SlideMedia from "../../components/SlideMedia/SlideMedia";
 import { CarouselHeader } from "./components";
 import styles from "./PageMedia.module.css";
-import SlideMedia from "../../components/SlideMedia/SlideMedia";
-import { v4 as uuidv4 } from "uuid";
 
 export default function PageMedia() {
   const loadingSlides = 10;
@@ -12,6 +12,7 @@ export default function PageMedia() {
       <header className={styles.header}>
         <CarouselHeader />
       </header>
+
       <main className={styles.main}>
         <CarouselResponsive XS={1.2} S={2.2} M={3.2} L={4.2} XL={5.2} XXL={6.2}>
           {Array.from({ length: loadingSlides }).map(() => (
