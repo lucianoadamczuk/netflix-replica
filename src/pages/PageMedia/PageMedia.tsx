60d94919389e9523bed2a3db83cea79ded8441ea
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-08-08 16:27:56 -0300

    feat(dependencies): :heavy_plus_sign: add uuid

--- src/pages/PageMedia/PageMedia.tsx
+++ src/pages/PageMedia/PageMedia.tsx
@@ -1,14 +1,23 @@
+import { SwiperSlide } from "swiper/react";
 import { CarouselResponsive } from "../../components";
 import { CarouselHeader } from "./components";
 import styles from "./PageMedia.module.css";
+import SlideMedia from "../../components/SlideMedia/SlideMedia";
 export default function PageMedia() {
+  const loadingSlides = 10;
   return (
     <>
       <header className={styles.header}>
         <CarouselHeader />
       </header>
       <main className={styles.main}>
-        <CarouselResponsive />
+        <CarouselResponsive XS={1.2} S={2.2} M={3.2} L={4.2} XL={5.2} XXL={6.2}>
+          {Array.from({ length: loadingSlides }).map(() => (
+            <SwiperSlide>
+              <SlideMedia />
+            </SwiperSlide>
+          ))}
+        </CarouselResponsive>
       </main>
     </>
   );
