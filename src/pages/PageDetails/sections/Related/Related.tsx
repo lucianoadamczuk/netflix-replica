5b9b930511f54f5a21657b8a3bcd950fc53dfd88
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-08-08 17:06:59 -0300

    feat(components): :sparkles: add CarouselResponsive to Related section in PageDetails

--- src/pages/PageDetails/sections/Related/Related.tsx
+++ src/pages/PageDetails/sections/Related/Related.tsx
@@ -1,14 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+import { SwiperSlide } from "swiper/react";
+import { CarouselResponsive } from "../../../../components";
 import styles from "./Related.module.css";
+import SlideMedia from "../../../../components/SlideMedia/SlideMedia";
 
 // TODO
 // add props
-// add carousel
 
 export default function Related() {
+  const loadingSlides = 10;
+
   return (
     <section className={styles.container}>
       <h5>Related</h5>
-      <div>future carousel</div>
+      <CarouselResponsive XS={1.2} S={2.2} M={3.2} L={4.2} XL={5.2} XXL={6.2}>
+        {Array.from({ length: loadingSlides }).map(() => (
+          <SwiperSlide key={uuidv4()}>
+            <SlideMedia />
+          </SwiperSlide>
+        ))}
+      </CarouselResponsive>
     </section>
   );
 }
