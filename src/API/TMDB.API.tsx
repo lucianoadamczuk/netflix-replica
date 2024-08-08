01a3bd721cd04a7156770cc2121d5dde3b9c13d7
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-08-08 18:56:27 -0300

    feat(API): :sparkles: add API TMDB

---
+++ src/API/TMDB.API.tsx
@@ -0,0 +1,55 @@
+const MOVIE_GENRES = [
+  { name: "Action", id: 28 },
+  { name: "Adventure", id: 12 },
+  { name: "Animation", id: 16 },
+  { name: "Comedy", id: 35 },
+  { name: "Crime", id: 80 },
+  { name: "Documentary", id: 99 },
+  { name: "Drama", id: 18 },
+  { name: "Family", id: 10751 },
+  { name: "Fantasy", id: 14 },
+  { name: "History", id: 36 },
+  { name: "Horror", id: 27 },
+  { name: "Music", id: 10402 },
+  { name: "Mystery", id: 9648 },
+  { name: "Romance", id: 10749 },
+  { name: "Science Fiction", id: 878 },
+  { name: "TV Movie", id: 10770 },
+  { name: "Thriller", id: 53 },
+  { name: "War", id: 10752 },
+  { name: "Western", id: 37 },
+];
+
+const SERIE_GENRES = [
+  { id: 10759, name: "Action & Adventure" },
+  { id: 16, name: "Animation" },
+  { id: 35, name: "Comedy" },
+  { id: 80, name: "Crime" },
+  { id: 99, name: "Documentary" },
+  { id: 18, name: "Drama" },
+  { id: 10751, name: "Family" },
+  { id: 10762, name: "Kids" },
+  { id: 9648, name: "Mystery" },
+  { id: 10763, name: "News" },
+  { id: 10764, name: "Reality" },
+  { id: 10765, name: "Sci-Fi & Fantasy" },
+  { id: 10766, name: "Soap" },
+  { id: 10767, name: "Talk" },
+  { id: 10768, name: "War & Politics" },
+  { id: 37, name: "Western" },
+];
+
+const IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+export const TMDB_API = {
+  base: "https://api.themoviedb.org/3/",
+  key: "?api_key=ce710ce9647f27d3c9514f1d3d3e1331",
+  poster_SM: IMAGE_URL + "w500",
+  poster_XL: IMAGE_URL + "original",
+  backdrop_SM: IMAGE_URL + "w780",
+  backdrop_XL: IMAGE_URL + "original",
+  moviesTrending: "trending/movie/week",
+  seriesTrending: "trending/tv/week",
+  movieGenres: MOVIE_GENRES,
+  serieGenres: SERIE_GENRES,
+};
